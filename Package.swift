d8b09000f0463fc08763a0e0a4b11653a6ea6c07
Thomas Lemay <thomas.lemay@miam.tech>
2024-07-17 09:45:19 +0200

    update release

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MealzCore",
+    defaultLocalization: "fr",
+    platforms: [
+        .iOS(.v12),
+        ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MealzCore",
+            targets: ["MealzCore"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "MealzCore",
+            url: "https://github.com/MealzStaging/MealzCoreRelease/raw/release/0.0.34/mealzcore.zip",
+            checksum: "c0c010db6805d2adab054d3728f798d4f1e6d34a3bcbff74eacba7bf3b4c8371"
+        )
+    ]
+)
