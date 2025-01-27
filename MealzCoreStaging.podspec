2e9b53407b8c2e7235cd5ccba870a73af49fcb6d
Thomas Lemay <thomas.lemay@miam.tech>
2025-01-27 13:49:59 +0100

    update release

---
+++ MealzCoreStaging.podspec
@@ -0,0 +1,17 @@
+
+Pod::Spec.new do |spec|
+    spec.name              = 'MealzCoreStaging'
+    spec.version           = '5.5.0'
+    spec.summary           = 'Mealz Core for iOS'
+    spec.homepage          = 'https://www.mealz.ai'
+       spec.description           = <<-DESC
+       Mealz Core SDK for iOS.
+       DESC
+    spec.author            = { 'Diarmuid McGonagle, Damien Walerowicz' => 'it@miam.tech' }
+    spec.license           = { :type => 'GPLv3', :file => 'LICENSE' }
+    spec.swift_versions = "5.8"
+    spec.platform          = :ios, "12.0"
+    spec.source            = { :http => 'https://github.com/MealzStaging/MealzCoreRelease/raw/release/5.5.0/mealzcore.zip' }
+    spec.ios.vendored_frameworks = 'mealzcore.xcframework'
+#    spec.resource_bundles = {'MealzCore' => ['*.xcprivacy']}
+end
---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MealzCore",
+    defaultLocalization: "fr",
+    platforms: [
+        .iOS(.v12),
+        ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MealzCore",
+            targets: ["MealzCore"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "MealzCore",
+            url: "https://github.com/MealzStaging/MealzCoreRelease/raw/release/5.5.0/mealzcore.zip",
+            checksum: "7e6a2570e34f761c6125acb235f29fd5ca702cf286dce99f944abbf5d0a8a115"
+        )
+    ]
+)
